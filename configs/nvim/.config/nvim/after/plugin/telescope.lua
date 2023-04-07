7516817c59afcbef56f1158a46472b644f42d8f3
Natombrio <pakke@DESKTOP-4UBQPAS>
2023-04-07 20:46:34 +0300

    Telescope startup config and ansible pacman unification

--- configs/nvim/.config/nvim/after/plugin/telescope.lua
+++ configs/nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,22 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n',"<C-p>", ":Telescope<CR>")
-vim.keymap.set('n',"<leader>ff", builtin.find_files, {})
-vim.keymap.set('n',"<leader>fi", builtin.live_grep, {})
-vim.keymap.set('n',"<leader>fg", builtin.git_files, {})
 
+-- list git files when in a git repo
+local project_files = function()
+  local opts = {} -- define here if you want to define something
+  vim.fn.system('git rev-parse --is-inside-work-tree')
+  if vim.v.shell_error == 0 then
+    require"telescope.builtin".git_files(opts)
+  else
+    require"telescope.builtin".find_files(opts)
+  end
+end
+
+if vim.fn.argc() == 0 then
+    vim.defer_fn(function()
+        project_files()
+    end, 0)
+end
+
+vim.keymap.set('n',"<leader>ff", project_files, {})
+vim.keymap.set('n',"<C-p>", ":Telescope<CR>")
+vim.keymap.set('n',"<leader>fi", builtin.live_grep, {})
