2fc65386f7d2e1f43c808cd25c8982392806b8b7
Natombrio <pakke@DESKTOP-4UBQPAS>
2023-04-11 00:49:38 +0300

    ansible configs

--- ansible/bin/ansible_setup.sh
+++ ansible/bin/ansible_setup.sh
@@ -4,4 +4,5 @@ set -e
 if ! [ -x "$(command -v ansible)" ]; then
     sudo pacman --noconfirm -Syu ansible
     sudo pacman --noconfirm -Syu ansible-core
+    ansible-galaxy collection install community.general
 fi
