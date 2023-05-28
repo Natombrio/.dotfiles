1a612bd951d272a10374e4394eecad929268471a
Natombrio <pakke@DESKTOP-4UBQPAS>
2023-05-29 01:16:38 +0300

    firefox script fix

--- configs/fish/.config/fish/functions/firefox.fish
+++ configs/fish/.config/fish/functions/firefox.fish
@@ -2,5 +2,5 @@
 
 function firefox --description "Launch windows firefox.exe"
     set launch_command 'C://Program Files/Mozilla Firefox/firefox.exe' $argv
-    windows_launch $launch_command
+    windows_launcher $launch_command
 end
