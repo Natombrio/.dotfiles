1ad809f0ca06d8d88381d4decc7fe6d8658137e7
Natombrio <>
2024-02-19 18:01:48 +0200

    changed fild_files bind

--- configs/nvim/.config/nvim/lua/plugins/telescope.lua
+++ configs/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         require('telescope').setup()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>f', builtin.find_files, {})
         vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
         vim.keymap.set('n', '<C-p>', builtin.builtin, {})
     end,
