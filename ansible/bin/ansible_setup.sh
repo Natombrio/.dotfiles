fc949e4f68cac6f6eacb552eba3fc908d960597f
Natombrio <11393974+Natombrio@users.noreply.github.com>
2025-05-20 15:27:58 +0300

    Remove ansible stuff, add vscode

--- ansible/bin/ansible_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if ! [ -x "$(command -v ansible)" ]; then
-    sudo pacman --noconfirm -Syu ansible
-    sudo pacman --noconfirm -Syu ansible-core
-    ansible-galaxy collection install community.general
-fi
--- ansible/install_everything.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./bin/ansible_setup.sh
-ansible-playbook --ask-become-pass ./tasks/01_wslconf.yml
-ansible-playbook --ask-become-pass ./tasks/02_configs.yml
-ansible-playbook --ask-become-pass ./tasks/03_pacmans.yml
-ansible-playbook ./tasks/04_cargos.yml
-
