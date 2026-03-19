69375c2d80e9091d6408d0cec1b3ed8036f7c55d
Natombrio <11393974+Natombrio@users.noreply.github.com>
2026-03-20 00:18:46 +0200

    Replace Telescope and mini_pick with snacks, remove trouble, add mini-icons.

---
+++ configs/common/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "folke/snacks.nvim",
+        ---@type snacks.Config
+        opts = {
+            picker = {}
+        },
+        keys = {
+            { "<C-S-f>", function() Snacks.picker.files() end, desc = "Find Files" },
+            { "<C-S-g>", function() Snacks.picker.grep() end, desc = "Find Live Grep" },
+            { "<C-p>", function() Snacks.picker.pick() end, desc = "Picker list" },
+        }
+    },
+}
