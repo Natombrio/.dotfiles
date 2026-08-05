428232865a9da7ed18ab0bdd3e535382f726df69
Natombrio <>
2026-08-05 18:40:55 +0300

    nvim deprecation fix

--- configs/common/.config/nvim/plugin/10_options.lua
+++ configs/common/.config/nvim/plugin/10_options.lua
@@ -76,7 +76,7 @@ autocmd("TextYankPost", {
     group = yank_group,
     pattern = "*",
     callback = function()
-        vim.hl.on_yank({
+        vim.hl.hl_op({
             higroup = "IncSearch",
             timeout = 40,
         })
